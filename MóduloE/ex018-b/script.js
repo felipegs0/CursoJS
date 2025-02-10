b8266686211fe633181de1d82c253cbe3593530d
devlip18 <felipegstech@gmail.com>
2025-02-10 20:34:56 -0300

    uptade

---
+++ MóduloE/ex018-b/script.js
@@ -0,0 +1,10 @@
+function calc() {
+    let n1 = parseFloat(document.getElementById('in1').value)
+    let n2 = parseFloat(document.getElementById('in2').value)
+    let n3 = parseFloat(document.getElementById('in3').value)
+
+    for 
+    let res = document.getElementById('res')
+
+
+}
---
+++ MóduloE/ex018/script.js
@@ -0,0 +1,10 @@
+function tab() {
+    var inum = document.getElementById('inum')
+    var num = parseInt('inum')
+    var res1 = document.getElementById('res')
+
+    res1.innerHTML = `${inum}`
+}
+
+
+
