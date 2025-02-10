4181f015c3ee2d0787d7a71cb60fd9845ecf6b0c
devlip18 <felipegstech@gmail.com>
2025-02-09 22:55:45 -0300

    uptade

--- MóduloD/ex014-b/script.js
+++ MóduloD/ex014-b/script.js
@@ -6,8 +6,15 @@ function verificar() {
     if (fano.value.length == 0 || Number(fano.value) > ano) {
         window.alert(`[ERRO] Verifique os dados e tente novamente!`)
     } else {
-        var fsex = document.getElementsByClassName('radsex')
+        var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
-        res.innerHTML = `Idade calculada: ${idade}`
+        var genero = ''
+        if (fsex[0].checked) {
+            genero = 'Homem'
+        } else {
+            genero = 'Mulher'
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos`
     }
 }
