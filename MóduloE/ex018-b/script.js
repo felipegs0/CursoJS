7fe4a05e0c3f7b0b70af0993275cdb6ff4188f8b
devlip18 <felipegstech@gmail.com>
2025-02-10 21:21:18 -0300

    uptade

--- MóduloE/ex018-b/script.js
+++ MóduloE/ex018-b/script.js
@@ -1,10 +1,19 @@
-function calc() {
-    let n1 = parseFloat(document.getElementById('in1').value)
-    let n2 = parseFloat(document.getElementById('in2').value)
-    let n3 = parseFloat(document.getElementById('in3').value)
-
-    for 
+function contar() {
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf') 
+    let passo = document.getElementById('txtp')
     let res = document.getElementById('res')
 
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        window.alert('[ERRO] Faltam dados!')
+    } else {
+        res.innerHTML = "Contando:"
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
 
+        for(let c = i; c <= f; c += p) {
+            res.innerHTML += `${c} `
+        }
+    }
 }
