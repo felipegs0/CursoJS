8b38a784b8eb0e1e89bc86fc84e5921811d1dae3
devlip18 <felipegstech@gmail.com>
2025-02-12 17:27:19 -0300

    uptade

--- MóduloE/ex018-b/script.js
+++ MóduloE/ex018-b/script.js
@@ -1,19 +1,18 @@
-function contar() {
-    let ini = document.getElementById('txti')
-    let fim = document.getElementById('txtf') 
-    let passo = document.getElementById('txtp')
-    let res = document.getElementById('res')
-
-    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-        window.alert('[ERRO] Faltam dados!')
+function tabuada() {
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+    if (num.value.length == 0) {
+        window.alert('Por favor, digite um número!')
+        
     } else {
-        res.innerHTML = "Contando:"
-        let i = Number(ini.value)
-        let f = Number(fim.value)
-        let p = Number(passo.value)
-
-        for(let c = i; c <= f; c += p) {
-            res.innerHTML += `${c} `
+        let n = Number(num.value)
+        let c = 1
+        tab.innerHTML = ''
+        while (c <= 10) {
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            tab.appendChild(item)
+            c++
         }
     }
 }
